473f0d07399aa4be8cf8b02995b6fc260e58eabd
EcitonTANG <ecitontang@protonmail.com>
2022-05-29 14:24:52 +0600

    Q

--- softfactory/settings.py
+++ softfactory/settings.py
@@ -77,12 +77,8 @@ WSGI_APPLICATION = 'softfactory.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'recipes',
-        'USER': 'postgres',
-        'PASSWORD': '6566',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
